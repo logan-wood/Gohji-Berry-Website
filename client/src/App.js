39211133ead3a6bc5e236ff577e6750690228b71
logan-wood <63706140+logan-wood@users.noreply.github.com>
2023-02-14 23:58:49 +1300

    clean frontend

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Nav() {
+    return (
+        <nav>
+            <h1>GOHJIBERRY</h1>
+            
+            <div>
+                <a href="#">home</a><br></br>
+                <a href="#">projects</a><br></br>
+                <a href="#">wip</a><br></br>
+                <a href="comics">comics</a><br></br>
+                <a href="#">commision</a><br></br>
+            </div>
+        </nav>
+
+        
+    );
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-import Nav from './Nav';
-import ContentList from './ContentList';
-import ComicList from './ComicList';
+import App from './App'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Nav />
-    <ComicList />
+    <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
