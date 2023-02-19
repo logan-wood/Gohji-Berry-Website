1a3d5c63b1ffb054decd015188e740ba69f781f0
logan-wood <63706140+logan-wood@users.noreply.github.com>
2023-02-19 19:25:06 +1300

    display-left-right function
    added buttons that can move the page to the side to show more content via jquery

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,65 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ComicList from './ComicList';
+import './App.css';
+import $ from 'jquery'
+
+function revealLeft() {
+    $('#center').children().css('display', 'none')
+    $('#left').animate({width: '80vw'}, function() {
+        $('#left').children().css('display', 'block')
+    })
+    $('#center').animate({width: '20vw'})
+}
+
+function closeLeft() {
+    $('#left').children().css('display', 'none')
+    $('#left').animate({width: '0vw'}, function() {
+        $('#center').children().css('display', 'block')
+    })
+    $('#center').animate({width: '100vw'})
+}
+
+function revealRight() {
+    $('#center').children().css('display', 'none')
+    $('#right').animate({width: '80vw'}, function() {
+        $('#right').children().css('display', 'block')
+    })
+    $('#center').animate({width: '20vw'})
+}
+
+function closeRight() {
+    $('#right').children().css('display', 'none')
+    $('#right').animate({width: '0vw'}, function() {
+        $('#center').children().css('display', 'block')
+    })
+    $('#center').animate({width: '100vw'})
+}
 
 export default function Nav() {
     return (
-        <nav>
-            <h1>GOHJIBERRY</h1>
-            
-            <div>
-                <a href="#">home</a><br></br>
-                <a href="#">projects</a><br></br>
-                <a href="#">wip</a><br></br>
-                <a href="comics">comics</a><br></br>
-                <a href="#">commision</a><br></br>
+        <>      
+            <div id='main'>
+                <section id='left'>
+                    <div id='leftContent'>
+                        <button onClick={closeLeft}>close</button>
+                        <br></br>Content listed here...
+                    </div>
+                </section>
+                <section id='center'>
+                    <div id='centerContent'>
+                        <h1>Main</h1>
+                        <button onClick={revealLeft}>reveal left</button><br></br>
+                        <button onClick={revealRight}>reveal right</button><br></br>
+                    </div>
+                </section>
+                <section id='right'>
+                    <div id='rightContent'>
+                        <button onClick={closeRight}>close</button>
+                        <br></br>Content listed here...
+                    </div>
+                </section>
             </div>
-        </nav>
-
-        
+        </>
     );
 }
