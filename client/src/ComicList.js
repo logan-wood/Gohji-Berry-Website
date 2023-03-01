28502735af44030fb84b44379d2d96a337044f45
logan-wood <63706140+logan-wood@users.noreply.github.com>
2023-03-01 21:42:59 +1300

    disable buttons while menu active

--- client/src/ComicList.js
+++ client/src/ComicList.js
@@ -1,6 +1,5 @@
-import React, {
-    
-} from 'react'
+import React from 'react'
+import './assets/styles/ComicList.css'
 
 class ComicList extends React.Component {
     state = {
@@ -28,38 +27,42 @@ class ComicList extends React.Component {
         } = this.state;
         return (
             <React.Fragment>
-                <h1>All Comics</h1>
-                {
-                    error ? <p> { error.message } </p> : null
-                } 
-                {
-                    comics.map(comic => {
-                        const {
-                            comic_id,
-                            comic_name,
-                            comic_description,
-                            file_paths
-                        } = comic
-                        //dynamically display pictures
-                        const file_paths_array = JSON.parse(file_paths)
-                        const pictures = file_paths_array.map(link => {
-                            return ( 
-                                <img key={comic_id} src={link} style={{width: '64px', height: 'auto'}} alt="artwork"></img> 
-                            )
-                        });
-                        
-                        return (
-                            <div key={comic_id}>
-                                <p>{comic_name}</p>
-                                <p>{comic_description}</p>
-                                {pictures}
-                                <br></br>
-                            </div>
-                        );
-                    })
+                <div className='comic-container'>
+                    <h1>All Comics</h1>
+
+                    {
+                        error ? <p> { error.message } </p> : null
+                    }
+
+                    {
+                        comics.map(comic => {
+                            const {
+                                comic_id,
+                                comic_name,
+                                comic_description,
+                                file_paths
+                            } = comic
+                            //dynamically display pictures
+                            const file_paths_array = JSON.parse(file_paths)
+                            const pictures = file_paths_array.map(link => {
+                                return ( 
+                                    <img key={comic_id} src={link} style={{width: '64px', height: 'auto'}} alt="artwork"></img> 
+                                )
+                            });
+                            
+                            return (
+                                <div key={comic_id}>
+                                    <p>{comic_name}</p>
+                                    <p>{comic_description}</p>
+                                    {pictures}
+                                    <br></br>
+                                </div>
+                            );
+                        })
 
 
-                }
+                    }
+                </div>
             </React.Fragment>
         );
     }
