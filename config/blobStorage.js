c9444566f5e9b059e02b1e28b5b3ab7dc51f3b1f
logan-wood <63706140+logan-wood@users.noreply.github.com>
2023-03-16 00:09:15 +1300

    File Upload Work

--- config/blobStorage.js
+++ config/blobStorage.js
@@ -7,32 +7,37 @@ const b2 = new B2({
 
 module.exports = {
     uploadFile: async (filename, buffer) => {
-        await b2.authorize().then(console.log('authorized'))
+        await b2.authorize().then(
+            // console.log('authorized')
+            )
 
         var uploadURL, uploadAuthToken
-        b2.getUploadUrl({
-            bucketId: process.env.BACKBLAZE_BUCKET_ID,
+        // console.log('getting upload url...')
+        await b2.getUploadUrl({
+            bucketId: process.env.BACKBLAZE_BUCKET_ID
         }).then((res) => {
             uploadURL = res.data.uploadUrl
             uploadAuthToken = res.data.authorizationToken
             // console.log(res.data)
-            console.log('promise resolved, uploadURL set')
-            console.log('attempting to upload file...')
-
-            b2.uploadFile({
-                uploadUrl: uploadURL,
-                uploadAuthToken: uploadAuthToken,
-                fileName: filename,
-                data: buffer
-            }).then((res) => {
-                console.log('file uploaded. displaying server response: ')
-                console.log(res.data)
-            }).catch((error) => {
-                console.log(error)
-            })
+            // console.log('promise resolved, uploadURL set')
+            
         })
 
-       
-        
+        var responseData
+        // console.log('attempting to upload file...')
+        await b2.uploadFile({
+            uploadUrl: uploadURL,
+            uploadAuthToken: uploadAuthToken,
+            fileName: filename,
+            data: buffer
+        }).then((res) => {
+            responseData = res.data
+            // console.log(res.data)
+        }).catch((error) => {
+            console.log(error)
+            throw error
+        })
+
+        return responseData
     }
 }
--- controllers/comicController.js
+++ controllers/comicController.js
@@ -4,7 +4,7 @@ const path = require('path')
 const blobStorage = require('../config/blobStorage')
 
 module.exports = {
-    getAllComics: (req, res) => {
+    getAllComics: async (req, res) => {
         db.query("SELECT * FROM comics", function (err, result) {
             if (err) throw err;
 
@@ -17,23 +17,23 @@ module.exports = {
         //declare variables
         const name = req.body.name, description = req.body.description;
         var files = [];
+        var b2FileNameArray = [];
 
         //populate fileURLs array
-        req.files.uploadFiles.forEach(file => {
+        for (const file of req.files.uploadFiles) {
             files.push({file: file.path, filename: file.filename})
-        });
+        }
 
         //upload files to blob storage
-        files.forEach(file => {
+        for (const file of files) {
             // get filepath
             fileRelativePath = path.join(__dirname, '..', '/uploads/comics/', file.filename)
-            
+                        
             // get file buffer
             const fileBuffer = fs.readFileSync(fileRelativePath)
 
             // upload file to storage
-            console.log('filename')
-            blobStorage.uploadFile(file.filename, fileBuffer).then(() => {
+            const fileUploadRes = await blobStorage.uploadFile(file.filename, fileBuffer).finally((res) => {
 
                 // once files have been uploaded to cloud, delete files in uploads folder
                 req.files.uploadFiles.forEach(file => {
@@ -41,20 +41,22 @@ module.exports = {
                         console.log(file.filename + ' deleted in ../uploads/comics')
                     })
                 })
-
-                // make new database entry
             })
-        })
+
+            //next, add the cloud file storage filename to an array for the database entry
+            b2FileNameArray.push(fileUploadRes.fileName)
+        }
         
+        console.log('making new database entry...')
+        // make new database entry
+        // convert b2FileNameArray into something friendly with the sql query
+        console.log(b2FileNameArray)
 
-        setTimeout(() => {
-            // delete files in uploads folder
-            req.files.uploadFiles.forEach(file => {
-                fs.unlink(path.join(__dirname, '..', 'uploads/comics/', file.filename), function() {
-                    console.log(file.filename + ' deleted in ../uploads/comics')
-                })
-            })
-        }, 10000)
+        // db.query('INSERT INTO comics (comic_name, comic_description, file_paths) VALUES (?, ?, ?)', [name, description, b2FileNameArray], (err, result) => {
+        //     if (err) throw err;
+
+        //     console.log(result)
+        // })
 
 
         res.sendStatus(200)
